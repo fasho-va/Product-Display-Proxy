d85cd79f38594a12753095aa6a2c6670247af4a5
MasonHN <hunternmason@gmail.com>
2019-06-03 14:17:23 -0500

    styling

--- public/proxy.js
+++ public/proxy.js
@@ -6,20 +6,20 @@ document.getElementsByClassName('ui button')[3].addEventListener('click', () => 
     window.scroll(0,0)
   } else {
     document.getElementById('add-to-bag').style.position = 'fixed'
-    console.log('here');
   }
-  console.log('here')
   document.getElementsByClassName('buttons')[0].style.opacity = '0'
   // document.getElementsByClassName('dWUjRn')[0].style.visibility = 'hidden'
   document.getElementById('add-to-bag').style.top = '0'
   document.getElementById('add-to-bag').style.right = '0'
   document.getElementById('add-to-bag').style.bottom = '0'
   document.getElementById('add-to-bag').style.zIndex = '3'
-  document.getElementsByClassName('sc-EHOje fxxRMC')[0].style.height = '100vh'
+  document.getElementsByClassName('sc-EHOje SkSPV')[0].style.height = '100vh'
   document.getElementById('overlay').style.visibility = 'initial'
   document.getElementById('overlay').style.opacity = '1'
   cartOpen = true;
 })
+
+
 
 document.getElementById('overlay').addEventListener('click', () => {
   if (cartOpen) {
@@ -29,7 +29,7 @@ document.getElementById('overlay').addEventListener('click', () => {
     document.getElementById('add-to-bag').style.top = 'initial'
     document.getElementById('add-to-bag').style.right = 'initial'
     document.getElementById('add-to-bag').style.bottom = 'initial'
-    document.getElementsByClassName('sc-EHOje fxxRMC')[0].style.height = 'initial'
+    document.getElementsByClassName('sc-EHOje SkSPV')[0].style.height = 'initial'
     document.getElementById('overlay').style.visibility = 'hidden'
     document.getElementById('overlay').style.opacity = '0'
     cartOpen = false;
