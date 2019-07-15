8baa36dc950d7a0e2eae8ff781965dc48e4d5ac0
MasonHN <hunternmason@gmail.com>
2019-07-15 14:52:11 -0500

    Adding Procfile

--- server.js
+++ server.js
@@ -1,15 +1,14 @@
-const express = require('express')
-const cors = require('cors')
-const bodyParser = require('body-parser')
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const port = process.env.PORT || 8000;
 
 const app = express();
 
 app.use(bodyParser());
 app.use(cors());
-app.use(express.static('./public'))
+app.use(express.static("./public"));
 
-
-
-app.listen(8000, () => {
-  console.log('listening on port 8000')
-})
+app.listen(port, () => {
+  console.log("listening on port", port);
+});
