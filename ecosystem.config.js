b7ff1a7de7f2c05faa1562468549fbbdf62f8dd1
MasonHN <hunternmason@gmail.com>
2019-07-21 11:16:32 -0500

    Changing urls

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,17 +1,19 @@
 module.exports = {
-  apps: [{
-    name: 'fashova',
-    script: './server.js'
-  }],
+  apps: [
+    {
+      name: "fashova",
+      script: "./server.js"
+    }
+  ],
   deploy: {
     production: {
-      user: 'ubuntu',
-      host: 'ec2-3-19-76-115.us-east-2.compute.amazonaws.com',
-      key: '~/.ssh/FashovaProxy.pem',
-      ref: 'origin/master',
-      repo: 'git@github.com:fasho-va/Product-Display-Proxy.git',
-      path: '/home/ubuntu/Fashova-Proxy',
-      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+      user: "ubuntu",
+      host: "ec2-18-222-172-209.us-east-2.compute.amazonaws.com",
+      key: "~/.ssh/Product-Display.pem",
+      ref: "origin/master",
+      repo: "git@github.com:fasho-va/Product-Display-Proxy.git",
+      path: "/home/ubuntu/Fashova-Proxy",
+      "post-deploy": "npm install && pm2 startOrRestart ecosystem.config.js"
     }
   }
-}
+};
